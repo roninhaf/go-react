a2497276411b6823e7ecb8b975b81bd61a39dfeb
Hafid Feghouli <feghouli.hafid@gmail.com>
2023-12-06 14:28:08 -0500

    removed useEffect

--- test-app/src/AppContent.jsx
+++ test-app/src/AppContent.jsx
@@ -4,18 +4,15 @@ const url = "https://jsonplaceholder.typicode.com/posts";
 export default function AppContent() {
   const [users, setUsers] = useState([]);
 
-  // useEffect(() => {
-    const fetchList = async () => {
-      try {
-        const response = await fetch(url);
-        const users = await response.json();
-        setUsers(users);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    // fetchList();
-  // }, []);
+  const fetchList = async () => {
+    try {
+      const response = await fetch(url);
+      const users = await response.json();
+      setUsers(users);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
